76030cf874f4e0016797109a01d76349aac84579
Jeremie Trussart <jeremie@jtrust.be>
2022-11-03 15:07:33 +0100

    SEPA QR Code

---
+++ src/main/java/be/jtrust/library/sepa/epccode/SepaQrCode.java
@@ -0,0 +1,53 @@
+package be.jtrust.library.sepa.epccode;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import lombok.Builder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Base64;
+
+@Builder
+public class SepaQrCode {
+
+
+    private String recipient;
+    private String iban;
+    private String currency;
+    private BigDecimal amount;
+    private String communication;
+
+
+    public String generateBase64Jpeg() {
+
+        var content =
+                """
+                BCD
+                002
+                1
+                SCT
+                                
+                %s
+                %s
+                %s%s
+                                
+                %s
+                """;
+
+        var qrCodeStringValue = String.format(content, recipient, iban, currency, amount.toPlainString(), communication);
+
+        try(ByteArrayOutputStream image = new ByteArrayOutputStream()) {
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            BitMatrix bitMatrix = qrCodeWriter.encode(qrCodeStringValue, BarcodeFormat.QR_CODE, 150, 150);
+            MatrixToImageWriter.writeToStream(bitMatrix,"jpeg", image);
+            return Base64.getEncoder().encodeToString(image.toByteArray());
+        } catch (IOException | WriterException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
